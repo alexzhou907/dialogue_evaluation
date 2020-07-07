53c5978724972d8c4faed9b7eff4af93893c9734
WinnieHAN <hanwj0309@gmail.com>
2020-07-07 22:35:21 +0800

    logic_reproduce

--- roberta_mnli/logic_eval_interface.py
+++ roberta_mnli/logic_eval_interface.py
@@ -1,6 +1,7 @@
 import os, sys
 import codecs
 import json
+import shutil
 from roberta_mnli.self_logic.MNLI_SCORE.make_wordnet_para_interface import make_wordnet_para_intf
 from roberta_mnli.self_logic.MNLI_SCORE.compute_score_interface import compute_score_intf
 
@@ -22,6 +23,7 @@ def logic_eval(sentences_pre, sentences):
     # Put sampled data in 'self_logic/MNLI_SCORE/data/{}/to_label_{}.txt'.format(mode)
     intermef = os.path.join('roberta_mnli', 'self_logic', 'MNLI_SCORE', 'data', mode, 'to_label_{}.txt'.format(mode)) # 'roberta_mnli/self_logic/MNLI_SCORE/data/{}/to_label_{}.txt'.format(mode, mode)
     dev_matched_tsv = os.path.join('roberta_mnli', 'self_logic', 'MNLI_SCORE', 'data', mode, 'dev_matched.tsv'.format(mode))  #'roberta_mnli/examples/data/self_logic/{}/dev_matched.tsv'.format(mode)
+    dev_mismatched_tsv = os.path.join('roberta_mnli', 'self_logic', 'MNLI_SCORE', 'data', mode, 'dev_mismatched.tsv'.format(mode)) 
     samples_json = os.path.join('roberta_mnli', 'self_logic', 'MNLI_SCORE', 'data', mode, 'samples.json') #'roberta_mnli/self_logic/MNLI_SCORE/data/{}/samples.json'.format(mode)
     intermepath = os.path.join('roberta_mnli', 'self_logic', 'MNLI_SCORE', 'data', mode) #'roberta_mnli/examples/data/self_logic/{}'.format(mode)
     score_file = os.path.join('roberta_mnli', 'self_logic', 'MNLI_SCORE', 'data', mode, 'mnli_{}.pred'.format(mode)) #'self_logic/MNLI_SCORE/data/{}/mnli.pred'.format(mode)
@@ -34,6 +36,7 @@ def logic_eval(sentences_pre, sentences):
     make_wordnet_para_intf(intermef, dev_matched_tsv, samples_json)
 
     # run MNLI. Output 'self_logic/MNLI_SCORE/data/{}/mnli.pred'.format(mode)
+    shutil.copyfile(dev_matched_tsv, dev_mismatched_tsv)
     commandf = os.path.join('roberta_mnli', 'examples', 'run_glue.py')
     os.system('python '+commandf+' --model_type roberta --model_name_or_path roberta-large-mnli --task_name MNLI --do_eval --do_lower_case --data_dir '+intermepath+' --max_seq_length 128 --per_gpu_eval_batch_size=8 --per_gpu_train_batch_size=8 --learning_rate 2e-5 --num_train_epochs 3.0 --output_dir roberta_test1 --print_result --overwrite_cache')
 
